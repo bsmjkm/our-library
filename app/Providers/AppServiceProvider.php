33f303ff3166c1abe4861b984dee2864209edf7b
Jakeem Bismaputra <jakeembisma@gmail.com>
2025-12-18 23:58:25 +0700

    Fix mixed content and force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\pagination\paginator;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL; // <-- Penting: Import Facade URL
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,6 +25,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Menggunakan style Bootstrap untuk pagination
         Paginator::useBootstrap();
+
+        // Memaksa HTTPS saat di Production (Vercel)
+        // Ini obat untuk tampilan yang hancur/tidak memuat CSS
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
